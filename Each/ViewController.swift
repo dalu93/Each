e571f5fccec98184606c83735fb22f274b702819
Luca D'Alberti <dalberti.luca93@gmail.com>
2016-10-14 09:41:24 +0200

    Initial Commit

---
+++ Each/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Each
+//
+//  Created by Luca D'Alberti on 10/14/16.
+//  Copyright © 2016 dalu93. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
