458df2249d1fefd7b3016c1ee325e7c5e80b22a7
Vincent Saluzzo <vincent.saluzzo@itelios.com>
2016-11-08 11:57:15 +0100

    migrate project to be framework/carthage compatible

---
+++ Each/Each-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Each-iOS.h
+//  Each-iOS
+//
+//  Created by Vincent Saluzzo on 08/11/2016.
+//  Copyright © 2016 dalu93. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Each-iOS.
+FOUNDATION_EXPORT double Each_iOSVersionNumber;
+
+//! Project version string for Each-iOS.
+FOUNDATION_EXPORT const unsigned char Each_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Each_iOS/PublicHeader.h>
+
+
