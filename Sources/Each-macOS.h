6a3b6c2ef4871fea63ad3efd072c9138f178fca2
Benjamin Snider <stupergenius@gmail.com>
2016-12-03 15:26:18 -0700

    Fixing macOS build.

---
+++ Sources/Each-macOS.h
@@ -0,0 +1,17 @@
+//
+//  Each-macOS.h
+//  Each
+//
+//  Created by Benjamin Snider on 12/3/16.
+//  Copyright © 2016 dalu93. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for deleteme.
+FOUNDATION_EXPORT double deletemeVersionNumber;
+
+//! Project version string for deleteme.
+FOUNDATION_EXPORT const unsigned char deletemeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <deleteme/PublicHeader.h>
